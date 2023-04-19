13e9c2e3db984cbf079b7ef3ca8d19a25f66edd9
Ghost-Clown <42879178+Ghost-Clown@users.noreply.github.com>
2023-04-19 17:01:40 +0100

    Add files via upload
    Added calibration files for the PG1800 grating at three angles

--- calibrate_arc_spectrum.py
+++ calibrate_arc_spectrum.py
@@ -11,7 +11,10 @@ from specidentify import WavelengthSolution
 from specidentify import ArcIdentify
 
 # enter the name of your arc spectrum here
-hdu = fits.open('cuar32.fits')
+#hdu = fits.open('cuar24125.fits')
+#hdu = fits.open('cuar2525.fits')
+hdu = fits.open('cuar26375.fits')
+#hdu = fits.open('cuar32.fits')
 #hdu = fits.open('cuar305.fits')
 arc = Spectrum1D(spectral_axis=hdu[1].data['pixel']*u.pixel, flux=hdu[1].data['counts']*u.ct)
 
@@ -22,7 +25,7 @@ ax.set_xlabel("Dispersion [pixels]")
 ax.set_ylabel("Flux")
 
 # enter the name of your calibration line list file here
-line_table = np.loadtxt('CuAr.txt', unpack=True, usecols=(0,1))
+line_table = np.loadtxt('calib_cuar.txt', unpack=True, usecols=(0,1))
 line_table = Table(line_table.T, names=('pixel', 'flux'))
 
 xarr = np.arange(len(arc.data))
@@ -34,9 +37,18 @@ ws_init = mod.fitting.LinearLSQFitter()(ws_init, xarr, xarr)
 ws = WavelengthSolution(x=None, wavelength=None, model=ws_init)
 
 # w should be selected lines from the calibration file, x should be their locations in the arc spectrum
+#24.125
+#x = [607,673,732,789,1089,1541,1616,1756,1862,2191,2284,2356,2549,2668,2771,3064]
+#w = [4072.385,4103.912,4131.724,4158.591,4300.101,4510.733,4545.052,4609.567,4657.901,4806.021,4847.81,4879.864,4965.08,5017.163,5062.037,5187.746]
+#25.25
+#x = [186,252,310,367,664,1113,1187,1326,1431,1665,1755,1847,1919,2227,2329,2616]
+#w = [4072.385,4103.912,4131.724,4158.591,4300.101,4510.733,4545.052,4609.567,4657.901,4764.865,4806.021,4847.81,4879.864,5017.163,5062.037,5187.746]
+#26.375
+x = [238,684,757,895,1000,1231,1321,1413,1483,1672,1788,1888,2173,2890,3041]
+w = [4300.101,4510.733,4545.052,4609.567,4657.901,4764.865,4806.021,4847.810,4879.864,4965.08,5017.163,5062.037,5187.746,5495.874,5524.957]
 #32
-x = [72,160,238,313,713,1323,1425,1616,1762,2087,2215,2344,2445,2717,2886]
-w = [4072.385,4103.912,4131.724,4158.591,4300.101,4510.073,4545.052,4609.567,4657.901,4764.865,4806.021,4847.810,4879.864,4965.080,5017.163]
+#x = [72,160,238,313,713,1323,1425,1616,1762,2087,2215,2344,2445,2717,2886]
+#w = [4072.385,4103.912,4131.724,4158.591,4300.101,4510.073,4545.052,4609.567,4657.901,4764.865,4806.021,4847.810,4879.864,4965.080,5017.163]
 #30.5
 #x = [631,720,799,875,1280,1898,2002,2197,2346,2678,2804,2942,3046]
 #w = [4072.385,4103.912,4131.724,4158.591,4300.101,4510.733,4545.052,4609.567,4657.901,4764.865,4806.021,4847.810,4879.864]
@@ -45,12 +57,16 @@ ws = WavelengthSolution(x=x, wavelength=w, model=ws_init)
 ws.fit()
 
 # use wavelength 4100-5100 for 32, 3800-4900 for 30.5
-aw = ArcIdentify(arc, ws, line_table, wavelength_range=[4100, 5100], flatten_order=9)
+# use 3800-5200 for 24.125, 4000-5400 for 25.25, 4200-5600 for 26.375
+aw = ArcIdentify(arc, ws, line_table, wavelength_range=[4200, 5600], flatten_order=9)
 aw.show_commands()
 aw.draw_error()
 aw.ws.sigma(aw.xp, aw.wp)
 
-hdu = fits.open('cuar32.fits')
+#hdu = fits.open('cuar24125.fits')
+#hdu = fits.open('cuar2525.fits')
+hdu = fits.open('cuar26375.fits')
+#hdu = fits.open('cuar32.fits')
 #hdu = fits.open('cuar305.fits')
 agn = Spectrum1D(spectral_axis=aw.ws(hdu[1].data['pixel'])*u.angstrom, flux=hdu[1].data['counts']*u.ct)
 
